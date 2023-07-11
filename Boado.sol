3f9d1794adc663dc47fbd26819fffdeeadd95f0f
ellaboadoo <138651079+ellaboadoo@users.noreply.github.com>
2023-07-11 19:34:50 +0800

    Update Boado.sol

--- Boado.sol
+++ Boado.sol
@@ -1,1 +1,24 @@
+contract MyToken {
 
+   
+   string public tokenName = "MANAYAM"; 
+   string public tokenAbbrv = "MNM";
+   uint public totalSupply = 0;
+   
+    
+   mapping(address => uint) public balances;
+
+    
+   function mint (address Taddress , uint Tvalue) public {
+      totalSupply += Tvalue;
+      balances[Taddress] += Tvalue;
+   }
+   
+    
+   function burn (address Taddress , uint Tvalue) public {
+      if (balances[Taddress] >= Tvalue) {
+      totalSupply -= Tvalue;
+      balances[Taddress] -= Tvalue;
+      }
+   }
+}
